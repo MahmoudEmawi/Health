0ad14cfeef736f1b7954b034ebde71bf7f603671
Muhannad Barhoum <MuhannadOBarhoum@gmail.com>
2022-06-18 07:26:49 +0300

    Update config.php

--- HealthCare/include/config.php
+++ HealthCare/include/config.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_SERVER','us-cdbr-east-05.cleardb.net');
-define('DB_USER','b56702afc843f9');
-define('DB_PASS' ,'afda8907');
-define('DB_NAME', 'health-care');
+define('DB_SERVER','ec2-54-157-16-196.compute-1.amazonaws.com:5432');
+define('DB_USER','vvhmdnzdzcqhtm');
+define('DB_PASS' ,'1822ba4e5b23b328cc0e1b0a7c197473b61c1ebb69073b50c49ee513ea3ac80e');
+define('DB_NAME', 'dmkkahi0skp5p');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
 if (mysqli_connect_errno())
