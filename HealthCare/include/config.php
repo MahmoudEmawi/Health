3e46cdb8d39ae28875af012cef93b5332595876a
Muhannad Barhoum <MuhannadOBarhoum@gmail.com>
2022-06-18 07:24:20 +0300

    Update config.php

--- HealthCare/include/config.php
+++ HealthCare/include/config.php
@@ -1,6 +1,5 @@
 <?php
 define('DB_SERVER','us-cdbr-east-05.cleardb.net');
-mysql://:@/heroku_a2b91699957dc8b 
 define('DB_USER','b56702afc843f9');
 define('DB_PASS' ,'afda8907');
 define('DB_NAME', 'health-care');
