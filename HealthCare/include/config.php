09cf3dd22a4ec7810837a549aa829a4444fbaff2
Muhannad Barhoum <MuhannadOBarhoum@gmail.com>
2022-06-18 07:23:49 +0300

    Update config.php

--- HealthCare/include/config.php
+++ HealthCare/include/config.php
@@ -1,7 +1,8 @@
 <?php
-define('DB_SERVER','postgres://vvhmdnzdzcqhtm:1822ba4e5b23b328cc0e1b0a7c197473b61c1ebb69073b50c49ee513ea3ac80e@ec2-54-157-16-196.compute-1.amazonaws.com:5432/dmkkahi0skp5p');
-define('DB_USER','vvhmdnzdzcqhtm');
-define('DB_PASS' ,'1822ba4e5b23b328cc0e1b0a7c197473b61c1ebb69073b50c49ee513ea3ac80e');
+define('DB_SERVER','us-cdbr-east-05.cleardb.net');
+mysql://:@/heroku_a2b91699957dc8b 
+define('DB_USER','b56702afc843f9');
+define('DB_PASS' ,'afda8907');
 define('DB_NAME', 'dmkkahi0skp5p');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
